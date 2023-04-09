593892e3a74aceef5bd76a2b8ab5604f78b4b713
MikalaiKlepchukou <mikalai.klepchukou@livewire.com>
2023-04-09 21:21:59 +0300

    feat: add task 4

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
