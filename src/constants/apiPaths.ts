5f2494fb6c3ed56bc0fe597a18afbbe5f61a7d15
Mikalai Klepchukou <MKlepchu@its.jnj.com>
2023-04-18 16:48:14 +0300

    feat: add task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
-  import: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://n9zlsoj84h.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
 };
