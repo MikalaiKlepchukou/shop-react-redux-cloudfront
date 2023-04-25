cbebffccca446e952f8eee6e6cb01a5b274bfd7b
MikalaiKlepchukou <108654942+MikalaiKlepchukou@users.noreply.github.com>
2023-04-25 23:49:50 +0300

    feat: update urls in task 6 (#5)
    Co-authored-by: Mikalai Klepchukou <MKlepchu@its.jnj.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
-  order: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
+  product: "https://m0v60z1nwf.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://m0v60z1nwf.execute-api.us-east-1.amazonaws.com/dev",
   import: "https://n9zlsoj84h.execute-api.us-east-1.amazonaws.com/dev",
-  bff: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
-  cart: "https://j7x45oqwpa.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://m0v60z1nwf.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://m0v60z1nwf.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
